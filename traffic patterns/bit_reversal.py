5cf82979511f1d851eaaa211659e1fb59f2ed8a4
Justin Dang <justinthdang@gmail.com>
2025-05-30 16:09:38 -0400

    add more error handling

--- traffic patterns/bit_reversal.py
+++ traffic patterns/bit_reversal.py
@@ -4,18 +4,20 @@ def getUserInput():
     number_of_gates = int(input("Number of gates: "))
 
     probabilities = [] # initialize list for storing probabilities
-    total_prob = 0 # initialize total probability
+    total_prob = 0 # initialize total probability tracker
     n = 1 # initialize count for n-qubits
     
-    # loops until total probability equals 1
-    while total_prob != 1:
+    # loops until total probability equals 1 or the number probabilities exceeds the number of gates
+    while total_prob != 1 and n <= number_of_gates:
         n_qubit_gate_prob = float(input(f"{n}-qubit gate probability: "))
-
-        total_prob += n_qubit_gate_prob # increment counter
 
         if not (0 <= n_qubit_gate_prob <= 1):
             print(f"{n}-qubit gate probability must be less between or equal to 0 and 1.")
             return
+        
+        # increment
+        total_prob += n_qubit_gate_prob
+        n += 1
         
         if total_prob > 1:
             print("Total probability must be 0 or 1.")
